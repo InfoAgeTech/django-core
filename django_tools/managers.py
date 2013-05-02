f889e8a40a85ff565bd924769871f92640d32801
Troy Grosfield <troy.grosfield@gmail.com>
2013-05-01 22:34:49 -0600

    Manager updates.

--- django_tools/managers.py
+++ django_tools/managers.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.http.response import Http404
+from django.template.defaultfilters import slugify
+from python_tools.random_utils import random_alphanum_id
 
 
-class CommonManager(models.Manager):
+class BaseManager(models.Manager):
+
+    def get_or_none(self, **kwargs):
+        """Gets a single object based on kwargs or None if one is not found."""
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+
+class CommonManager(BaseManager):
 
     def get_by_id(self, id, fields=None):
         """Gets a document by an id.
@@ -12,8 +24,7 @@ class CommonManager(models.Manager):
         :param fields: a tuple of field names to return.  If None, all fields 
             are returned (default).
         """
-        criteria = {'id': id}
-        return self._get_one(criteria=criteria)
+        return self.get_or_none(id=id)
 
     def get_by_id_or_404(self, id):
         """Gets by a instance instance r raises a 404 is one isn't found."""
@@ -33,21 +44,10 @@ class CommonManager(models.Manager):
         """
         return self.filter(id__in=ids)
 
-    def _get_one(self, **criteria):
-        """
-        Gets a single document based on a specific set of criteria.
-        
-        :param criteria: criteria for the query
-        :param fields: a tuple of field names to return.  If None, all fields 
-            are returned (default).
-        """
-        try:
-            return self.get(**criteria)
-        except self.model.DoesNotExist:
-            return None
-
     def bulk_create(self, objs, *args, **kwargs):
         """Insert many object at once."""
+        # TODO: This might be making a bad assumption that this is inheriting
+        # from AbstractBaseModel
         self.model.save_prep(instance_or_instances=objs)
         return super(CommonManager, self).bulk_create(*args, **kwargs)
 
@@ -67,3 +67,97 @@ class CommonManager(models.Manager):
             return True
         except self.model.DoesNotExist:
             return False
+
+
+class SlugManager(BaseManager):
+    """Manager mixin for slugs."""
+
+    def get_by_slug(self, slug, **kwargs):
+        try:
+            return self.get(slug=slug, **kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+    def get_by_slug_or_404(self, slug, **kwargs):
+        group = self.get_by_slug(slug, **kwargs)
+
+        if group:
+            return group
+
+        raise Http404
+
+    def is_slug_available(self, slug, **kwargs):
+        """Checks to see if a slug is available. If the slug is already being used
+        this method returns False.  Otherwise, return True.
+        """
+        try:
+            self.get(slug=slug, **kwargs)
+            return False
+        except self.model.DoesNotExist:
+            return True
+
+    def get_next_slug(self, slug, **kwargs):
+        """Gets the next available slug.
+        
+        :param slug: the slug to slugify
+        :param kwargs: additional filter criteria to check for when looking for
+            a unique slug.
+        
+        Example:
+        
+        if the value "my-slug" is already taken, this method will append "-n"
+        to the end of the slug until the next available slug is found.
+        
+        """
+        original_slug = slug = slugify(slug)
+        count = 0
+
+        while not self.is_slug_available(slug=slug, **kwargs):
+            count += 1
+            slug = '{0}-{1}'.format(original_slug, count)
+
+        return slug
+
+
+class TokenManager(BaseManager):
+    """Manager Mixin for tokens."""
+
+    def get_by_token(self, token, **kwargs):
+        """Get by token."""
+        return self.get_or_none(token=token, **kwargs)
+
+    def get_by_token_or_404(self, token, **kwargs):
+        group = self.get_by_token(token, **kwargs)
+
+        if group:
+            return group
+
+        raise Http404
+
+    def get_next_token(self, length=25, **kwargs):
+        """Gets the next available token.
+        
+        :param kwargs: additional filter criteria to check for when looking for
+            a unique token.
+        
+        """
+        while True:
+            tokens = [random_alphanum_id(id_len=length) for t in range(5)]
+            obj_ids = self.filter(token__in=tokens).values_list('id')
+            available = set(tokens).difference(obj_ids)
+
+            if available:
+                return available.pop()
+
+
+class UserManager(models.Manager):
+    """Manager Mixin for models that have a user."""
+
+    def get_by_user(self, user, **kwargs):
+        return self.get_by_user_id(user_id=user.id, **kwargs)
+
+    def get_by_user_id(self, user_id, **kwargs):
+        try:
+            return self.filter(user_id=user_id)
+        except self.model.DoesNotExist:
+            return []
