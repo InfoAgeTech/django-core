f947d298f47e907ea0e01999d1404ade466e3131
Troy Grosfield <troy.grosfield@gmail.com>
2013-10-02 19:11:40 -0600

    removed commented out code

--- django_core/mixins/common.py
+++ django_core/mixins/common.py
@@ -15,23 +15,3 @@ class CommonSingleObjectMixin(object):
         obj = super(CommonSingleObjectMixin, self).get_object(**kwargs)
         self.object = obj
         return obj
-
-# TODO this need to go away.  I can just call .select_related on the regular
-# queryset that gets returned.
-#     def get_queryset(self, select_related=False, **kwargs):
-#         """Get the related objects queryset.
-#
-#         :param select_related: if the related objects are wanting to
-#             be selected. (i.e. query will be made of all related objects). If
-#             True, this will select the immediate foreign key relations.
-#             Otherwise, this can be a string, list or tuple of additional fields
-#             to retrieve.  All of the following are valid:
-#
-#             >> get_queryset(select_related=True)
-#         """
-#         queryset = super(CommonSingleObjectMixin, self).get_queryset(**kwargs)
-#
-#         if select_related:
-#             return queryset.select_related()
-#
-#         return queryset
