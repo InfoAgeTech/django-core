1a9d95844351a0a4cbf801363e62e5c724449e8d
Troy Grosfield <troy.grosfield@gmail.com>
2013-10-11 07:08:58 -0600

    added make_iterable template tag

--- django_core/templatetags/collection_tags.py
+++ django_core/templatetags/collection_tags.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from django import template
 
-
 register = template.Library()
+
 
 @register.filter
 def getitem(d, key):
     """
     Ability to access a dictionary keys based on a dynamic key:
-    
+
     Usage:
-    
+
      my_vals = {'hello': 'world', 'testing': 'again'}
-    
+
      {{ my_vals|get:'hello' }}
-     
+
      would return "world"
     """
     if not isinstance(d, dict):
@@ -27,10 +27,28 @@ def getitem(d, key):
 def attr(obj, attr):
     """
     Does the same thing as getattr.
-    
+
     getattr(obj, attr, '')
     """
     if not obj or not hasattr(obj, attr):
         return ''
 
     return getattr(obj, attr, '')
+
+
+@register.filter
+def make_iterable(obj):
+    """Make an object iterable.
+
+    >>> make_iterable(obj='hello')
+    ('hello',)
+    >>> make_iterable(obj=None)
+    ()
+    """
+    if not obj:
+        return tuple()
+
+    if isinstance(obj, (list, tuple, set)):
+        return obj
+
+    return (obj,)
