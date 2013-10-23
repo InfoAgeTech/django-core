d733d5be429580a8131334d8332a35e725e5cf82
Troy Grosfield <troy.grosfield@gmail.com>
2013-10-22 23:20:39 -0600

    added math tags

---
+++ django_core/templatetags/math_tags.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def substract(value, subtract_by):
+    """
+    Get the absolute value for "value".  This template tag is a wrapper for
+    pythons "abs(...)" method.
+
+    Usage:
+
+    >>> absolute(5, 2)
+    3
+    """
+    return value - subtract_by
+
+
+@register.filter
+def multiply(value, multiplier):
+    """
+    Multiplies two values together.
+
+    Usage:
+
+    >>> multiply(5, 2)
+    10
+    """
+    return value * multiplier
+
+
+@register.filter
+def divide(numerator, denominator):
+    """
+    Divides two values from each other.
+
+    Usage:
+
+    >>> absolute(-5)
+    5
+    """
+    return numerator / denominator
+
+
+@register.filter
+def absolute(value):
+    """
+    Get the absolute value for "value".  This template tag is a wrapper for
+    pythons "abs(...)" method.
+
+    Usage:
+
+    >>> absolute(-5)
+    5
+    """
+    try:
+        return abs(value)
+    except:
+        return value
