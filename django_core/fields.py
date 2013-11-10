f65523345277c3a4a927b45c6421a186b3174dca
Troy Grosfield <troy.grosfield@gmail.com>
2013-11-10 10:31:37 -0700

    removed simplejson references

--- django_core/fields.py
+++ django_core/fields.py
@@ -4,9 +4,10 @@
 
 https://github.com/omab/django-social-auth/blob/master/social_auth/fields.py
 """
+import json
+
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils import simplejson
 from django.utils.encoding import smart_unicode
 
 
@@ -25,7 +26,7 @@ class JSONField(models.TextField):
             return None
         if isinstance(value, basestring):
             try:
-                return simplejson.loads(value)
+                return json.loads(value)
             except Exception, e:
                 raise ValidationError(str(e))
         else:
@@ -37,14 +38,14 @@ class JSONField(models.TextField):
         if isinstance(value, basestring):
             super(JSONField, self).validate(value, model_instance)
             try:
-                simplejson.loads(value)
+                json.loads(value)
             except Exception, e:
                 raise ValidationError(str(e))
 
     def get_prep_value(self, value):
         """Convert value to JSON string before save"""
         try:
-            return simplejson.dumps(value)
+            return json.dumps(value)
         except Exception, e:
             raise ValidationError(str(e))
 
