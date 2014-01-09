1598d4bb09b55733fd1c2356056aa82dbe79b5c2
Troy Grosfield <troy.grosfield@gmail.com>
2014-01-08 18:43:22 -0700

    Adding python3 support

--- django_core/models/fields.py
+++ django_core/models/fields.py
@@ -5,7 +5,7 @@ import json
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.encoding import smart_text
-from django.utils.encoding import smart_unicode
+from django.utils.six import string_types
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -157,10 +157,10 @@ class JSONField(models.TextField):
         """
         if self.blank and not value:
             return None
-        if isinstance(value, basestring):
+        if isinstance(value, string_types):
             try:
                 return json.loads(value)
-            except Exception, e:
+            except Exception as e:
                 raise ValidationError(str(e))
         else:
             return value
@@ -168,20 +168,20 @@ class JSONField(models.TextField):
     def validate(self, value, model_instance):
         """Check value is a valid JSON string, raise ValidationError on
         error."""
-        if isinstance(value, basestring):
+        if isinstance(value, string_types):
             super(JSONField, self).validate(value, model_instance)
             try:
                 json.loads(value)
-            except Exception, e:
+            except Exception as e:
                 raise ValidationError(str(e))
 
     def get_prep_value(self, value):
         """Convert value to JSON string before save"""
         try:
             return json.dumps(value)
-        except Exception, e:
+        except Exception as e:
             raise ValidationError(str(e))
 
     def value_to_string(self, obj):
         """Return value from object converted to string properly"""
-        return smart_unicode(self.get_prep_value(self._get_val_from_obj(obj)))
+        return smart_text(self.get_prep_value(self._get_val_from_obj(obj)))
