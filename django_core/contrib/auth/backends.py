e4a511387a27e4b64539a8daeda6a0f67c598f68
Troy Grosfield <troy.grosfield@gmail.com>
2014-02-05 06:33:21 -0700

    added email or username auth backend

---
+++ django_core/contrib/auth/backends.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django_core.utils.validators import is_valid_email
+
+
+User = get_user_model()
+
+
+class EmailOrUsernameBackend(ModelBackend):
+    """Authentication backend to allow a user to login via email address or
+    username.
+    """
+
+    def authenticate(self, username_or_email, password):
+        # If username is an email address, then try to pull it up
+        user = self.get_by_username_or_email(username_or_email)
+
+        if not user:
+            return None
+
+        if user.check_password(password):
+            return user
+
+        if User.check_password(password):
+            return user
+        return None
+
+    def get_by_username_or_email(self, username_or_email):
+        username_or_email = username_or_email.strip().lower()
+
+        if is_valid_email(username_or_email):
+            return self.get_by_email(username_or_email)
+
+        return self.get_by_username(username_or_email)
+
+    def get_by_email(self, email):
+        try:
+            return User.objects.get(email=email.lower())
+        except User.DoesNotExist:
+            return None
+
+    def get_by_username(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
