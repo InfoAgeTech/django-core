bd193008bae681ef0bfc4c3bd490b5df1b867bd2
Troy Grosfield <troy.grosfield@gmail.com>
2014-02-28 11:07:41 -0700

    adding html link element builder.

--- django_core/html/builders.py
+++ django_core/html/builders.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
@@ -9,25 +11,45 @@ def build_link(href, text, cls=None, icon_class=None, **attrs):
     :param text: text for the anchor element
     :param attrs: other attribute kwargs
 
-    >>> link('xyz.com', 'hello', 'big')
+    >>> build_link('xyz.com', 'hello', 'big')
     u'<a href="xyz.com" class="big">hello</a>'
-    >>> link('xyz.com', 'hello', 'big', 'fa fa-times')
+    >>> build_link('xyz.com', 'hello', 'big', 'fa fa-times')
     u'<a href="xyz.com" class="big"><i class="fa fa-times"></i> hello</a>'
     """
-    if not attrs:
-        attrs = {}
+    return build_html_element(tag='a',
+                              text=text,
+                              href=href,
+                              cls=cls,
+                              icon_class=icon_class,
+                              **attrs)
 
-    attrs['href'] = href
 
-    if cls:
-        attrs['class'] = cls
+def build_html_element(tag, text=None, icon_class=None, cls=None, **kwargs):
+    """Builds an html element.
 
-    attrs_formatted = u' '.join([u'{0}="{1}"'.format(k, v)
-                                 for k, v in attrs.items()])
+    :param tag: the html tag to build ('a', 'div', 'img', etc)
+    :param icon_class: the class to apply to an icon element.  This only
+        applies to elements that allow a closing tag.
+    :param cls: the css class to apply to the tag.  This can also be passed
+        in as a kwarg as "class".
 
-    icon = u'<i class="{0}"></i> '.format(icon_class) if icon_class else ''
+    >>> build_html_element(tag='a', href='someurl.com', text='hello')
+    '<a href='someurl.com'>hello</a>'
+    """
+    if cls is not None:
+        kwargs['class'] = cls
 
-    return mark_safe(u'<a {attrs}>{icon}{text}</a>'.format(
-                                                    attrs=attrs_formatted,
+    tag_attrs = ' '.join(['{0}="{1}"'.format(k, v) for k, v in kwargs.items()])
+
+    tag_content = '{tag} {tag_attrs}'.format(tag=tag, tag_attrs=tag_attrs)
+
+    if tag in ('img', 'input', 'hr', 'br'):
+        return mark_safe('<{tag_content} />'.format(tag_content=tag_content))
+
+    icon = '<i class="{0}"></i> '.format(icon_class) if icon_class else ''
+
+    return mark_safe('<{tag_content}>{icon}{text}</{tag}>'.format(
+                                                    tag_content=tag_content,
                                                     icon=icon,
-                                                    text=escape(text)))
+                                                    tag=tag,
+                                                    text=escape(text) or ''))
