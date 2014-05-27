337270003c4d5111287caf036900bbf77d042602
Troy Grosfield <troy.grosfield@gmail.com>
2014-05-27 07:57:05 -0500

    Added json hybrid CBVs

--- django_core/views/response.py
+++ django_core/views/response.py
@@ -7,6 +7,13 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.generic.base import View
 from django.views.generic.edit import UpdateView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import DeleteView
+from django.http.response import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.template.loader import render_to_string
+from django.utils.html import strip_spaces_between_tags
+from django.views.generic.list import ListView
 
 
 class JSONResponseMixin(object):
@@ -32,30 +39,117 @@ class JSONResponseMixin(object):
                             **kwargs)
 
 
-class JSONHybridUpdateView(JSONResponseMixin, UpdateView):
-    """Hybrid view that handles regular update requests as well as json update
-    requests.
-    """
+class JSONHybridTemplateView(JSONResponseMixin, TemplateView):
+
     def render_to_response(self, context):
 
         if self.request.is_ajax():
             return JSONResponseMixin.render_to_response(self, context)
 
-        return UpdateView.render_to_response(self, context)
+        return TemplateView.render_to_response(self, context)
+
+
+class JSONHybridProcessFormViewMixin(JSONResponseMixin):
+    """Hybrid mixin that handles form processing.
+
+    Fields:
+
+    json_template_name: if provided, this template will be used to render
+        an html template response that will be returned in the response data.
+
+    Example:
+
+    class MyView(JSONHybridProcessFormViewMixin, CreateView):
+        json_template_name = 'path/to/json_template.html'
+
+        def get_json_context_data(self, **kwargs):
+            context = super(MyView, self).get_json_context_data(**kwargs)
+            context['my_json_template_var'] = 'hello world'
+            return context
+
+    Successful JSON response (form is valid):
+
+    {
+        'html': '<div>Some rendererd html response "hello world"</div>'
+    }
+    """
+    json_template_name = None
 
     def form_valid(self, form):
         if self.request.is_ajax():
             self.object = form.save()
-            return JSONResponseMixin.render_to_response(self, context={})
+            context = {}
+            if self.json_template_name:
+                json_context = self.get_json_context_data(**{
+                    self.get_context_object_name(self.object): self.object
+                })
+                html = render_to_string(template_name=self.json_template_name,
+                                        dictionary=json_context)
+                context['html'] = strip_spaces_between_tags(html.strip())
 
-        return UpdateView.form_valid(self, form)
+            return JSONResponseMixin.render_to_response(self, context=context)
+
+        return super(JSONHybridProcessFormViewMixin, self).form_valid(form)
 
     def form_invalid(self, form):
         if self.request.is_ajax():
             context = {'errors': form.errors}
             return JSONResponseMixin.render_to_response(self, context)
 
-        return UpdateView.form_invalid(self, form)
+        return super(JSONHybridProcessFormViewMixin, self).form_invalid(form)
+
+    def get_json_context_data(self, **kwargs):
+        return kwargs or {}
+
+
+class JSONHybridCreateView(JSONHybridProcessFormViewMixin, CreateView):
+    """Hybrid view that handles regular create requests as well as json create
+    requests.
+    """
+    def render_to_response(self, context):
+
+        if self.request.is_ajax():
+            return JSONHybridProcessFormViewMixin.render_to_response(self,
+                                                                     context)
+
+        return CreateView.render_to_response(self, context)
+
+
+class JSONHybridUpdateView(JSONHybridProcessFormViewMixin, UpdateView):
+    """Hybrid view that handles regular update requests as well as json update
+    requests.
+    """
+    def render_to_response(self, context):
+
+        if self.request.is_ajax():
+            return JSONHybridProcessFormViewMixin.render_to_response(self,
+                                                                     context)
+
+        return UpdateView.render_to_response(self, context)
+
+
+class JSONHybridDeleteView(JSONResponseMixin, DeleteView):
+
+    def render_to_response(self, context):
+
+        if self.request.is_ajax():
+            return JSONResponseMixin.render_to_response(self, context)
+
+        return DeleteView.render_to_response(self, context)
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Calls the delete() method on the fetched object and then
+        redirects to the success URL.
+        """
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.delete()
+
+        if self.request.is_ajax():
+            return JSONResponseMixin.render_to_response(self, context={})
+
+        return HttpResponseRedirect(success_url)
 
 
 # TODO: this should go away in favor of JSONResponseMixin above
