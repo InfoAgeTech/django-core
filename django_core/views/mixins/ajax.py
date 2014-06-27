93356fbb8eb3dc50b00f0bdc6f1a1e6f8ff4f8d0
Troy Grosfield <troy.grosfield@gmail.com>
2014-06-27 15:13:21 -0600

    added doc.

--- django_core/views/mixins/ajax.py
+++ django_core/views/mixins/ajax.py
@@ -5,8 +5,10 @@ from django.template.context import RequestContext
 class AjaxViewMixin(object):
     """View mixin for alternatively returning a different template response
     when the request is made via ajax.
-    """
 
+    The consuming view needs to implement the ``ajax_template_name`` which is
+    the location to the template to use when an ajax call is made.
+    """
     ajax_template_name = None
 
     def render_to_response(self, context, **response_kwargs):
