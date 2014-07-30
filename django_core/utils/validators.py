434953767dbff5102a05873590ac280c1140d33f
Troy Grosfield <troy.grosfield@gmail.com>
2014-07-30 10:16:26 -0600

    added password strengh validator

--- django_core/utils/validators.py
+++ django_core/utils/validators.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from django.utils.translation import ugettext as _
 
 
 def is_valid_email(value):
@@ -12,3 +14,22 @@ def is_valid_email(value):
         return True
     except ValidationError:
         return False
+
+
+def validate_password_strength(value):
+    """Validates that a password is as least 7 characters long and has at least
+    1 digit and 1 letter.
+    """
+    min_length = 7
+
+    if len(value) < min_length:
+        raise ValidationError(_('Password must be at least {0} characters '
+                                'long.').format(min_length))
+
+    # check for digit
+    if not any(char.isdigit() for char in value):
+        raise ValidationError(_('Password must container at least 1 digit.'))
+
+    # check for letter
+    if not any(char.isalpha() for char in value):
+        raise ValidationError(_('Password must container at least 1 letter.'))
