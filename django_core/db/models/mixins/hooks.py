a31b5d6aaa3fa41613191a0206bb0d79bad54e0a
Troy Grosfield <troy.grosfield@gmail.com>
2014-08-08 08:57:09 -0600

    added doc to the model hook

--- django_core/db/models/mixins/hooks.py
+++ django_core/db/models/mixins/hooks.py
@@ -3,13 +3,19 @@ from django.db import models
 
 class AbstractHookModelMixin(models.Model):
     """Hook for adding additional functionality to a model without having to
-    define a new model (proxy or otherwise).  This model mixin merely acts as a
-    placeholder for adding things like project specific methods like url
-    helpers:
+    define a new model (proxy or otherwise).  The primary benefit here is if
+    you wanted to add additional fields to a model.  A proxy model won't allow
+    for that and non abstract models that are extended require additional
+    database lookups to satisfy the one-to-one relationship.
 
-    * get_absolute_url(...)
+    This is useful in third party applications where you want to extend the
+    functionality of a model, but don't own the code.
 
-    If isn't overridden, then everythings functions like normal.
+    If it isn't overridden, then everything functions like normal.
+
+    For an example usage, see:
+
+    https://github.com/InfoAgeTech/django-user-connections/blob/master/django_user_connections/models.py
     """
     class Meta:
         abstract = True
