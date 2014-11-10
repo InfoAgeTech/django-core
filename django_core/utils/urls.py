36ae88cdc6b21bdb7c29fe03ae1d267f1ae39577
Troy Grosfield <troy.grosfield@gmail.com>
2014-11-10 12:59:07 -0700

    added build_url method that constructs a url and properly encodes querystring params.

--- django_core/utils/urls.py
+++ django_core/utils/urls.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.shortcuts import redirect
+from django.utils.http import urlencode
 
 
 def is_legit_next_url(next_url):
@@ -27,3 +28,16 @@ def safe_redirect(next_url, default=None):
         return redirect(default)
 
     return redirect('/')
+
+
+def build_url(url, querystring_params=None):
+    """Builds a url string with properly encoded queryparams.
+    
+    :params url: the primary url with no querystring params.  I.E. 
+        http://somesite.com/path/to/page
+    :param querystring_params: dict of querystring key value pairs.    
+    """
+    if not querystring_params:
+        return url
+
+    return '{0}?{1}'.format(url, urlencode(querystring_params))
