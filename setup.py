d267ddd7c7b0178767d0eb27856fd157f54e4627
Troy Grosfield <troy.grosfield@gmail.com>
2015-04-19 21:59:45 -0600

    classifiers updates

--- setup.py
+++ setup.py
@@ -13,8 +13,10 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Internet :: WWW/HTTP',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
 ]
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
