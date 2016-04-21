cccbaa622493d086a49d30e9a6a7dbd7a90561b1
Troy Grosfield <troy.grosfield@gmail.com>
2016-04-21 09:53:03 -0600

    linebreak safe updates.

--- django_core/templatetags/html_tags.py
+++ django_core/templatetags/html_tags.py
@@ -1,18 +1,18 @@
 from django import template
-from django.utils.html import linebreaks
-from django.utils.safestring import mark_safe
+from django.template.defaultfilters import linebreaks_filter
 from django.utils.six import string_types
 
 
+# from django.utils.html import linebreaks
 register = template.Library()
 
 
 @register.filter
-def linebreaks_safe(value):
+def linebreaks_safe(value, autoescape=True):
     """
     Adds linebreaks only for text that has a newline character.
     """
     if isinstance(value, string_types) and '\n' in value:
-        return mark_safe(linebreaks(value))
+        return linebreaks_filter(value, autoescape=autoescape)
 
     return value
